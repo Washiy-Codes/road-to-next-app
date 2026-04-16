dd5e7a19af887a7544c7a51233376d42821fc8ad
Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-04-16 22:32:35 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-linear-to-br from-purple-400 to-pink-600">
+      <h1 className="font-red py-20px text-center">Here to learn</h1>
+    </div>
+  )
+}
+
+export default page
