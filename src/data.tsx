2fc28efc08c995547b09f17ccffe03433819ab58
Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-05-01 22:58:54 +0300

    refactor(ticket): move types to dedicated file, rename to .ts, and standardize Status type usage

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,6 @@
-const Data: { id: number; title: string, status: string; content: string}[] = [
+import type { Status } from "@/features/ticket/components/type";
+
+const Data: { id: number; title: string, status: Status; content: string}[] = [
     { id: 1, 
       title: "Ticket 1", 
       status:"DONE", 
@@ -9,7 +11,8 @@ const Data: { id: number; title: string, status: string; content: string}[] = [
      id: 2, 
      title: "Ticket 2",
      status: "IN_PROGRESS",
-     content: "This is the second ticket"
+     content: "This is the second ticket",
+
     },
 
     { id: 3, 
